ce3631c1d16148dc65694d321e09b47630916adf
teste <mauriciolor@hotmail.com>
2024-08-26 19:11:02 -0300

    adicionado exercicio 10

--- exercicios.py
+++ exercicios.py
@@ -1,4 +1,15 @@
-numero_01 = int(input("Inserir um número inteiro:"))
-numero_02 = int(input("Inserir um número inteiro:"))
-resultado = numero_01 // numero_02
-print(resultado)
+
+import math
+
+#Exercicio 04
+
+#numero_01 = int(input("Inserir um número inteiro:"))
+#numero_02 = int(input("Inserir um número inteiro:"))
+#resultado = numero_01 // numero_02
+# print(resultado)
+
+#Exercicio 10
+
+raio_do_circulo = float(input("Digite o raio:"))
+area_do_circulo = math.pi * raio_do_circulo ** 2
+print(f"{area_do_circulo:.2f}")
